3fa404d5e00612265b000ffa59d48b890d5ec0f9
ayush3242 <pacerayush90@gmail.com>
2024-07-18 22:36:05 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "GAMING"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Gamechain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "GM"
 )
 
 var ID ids.ID
